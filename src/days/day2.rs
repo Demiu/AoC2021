95bb8180f0f495569c7a947efd4e14131ffc2bee
Demiu <miazgapow@gmail.com>
2021-12-02 10:05:20 +0100

    Improve day 2 part 2
    Replace direction magnitude tuple with a command enum containing values

--- src/days/day2.rs
+++ src/days/day2.rs
@@ -2,12 +2,11 @@ use crate::util::*;
 
 type SolverInput = Vec<Command>;
 
-pub enum Direction {
-    Down,
-    Forward,
-    Up,
+pub enum Command {
+    Down(u32),
+    Forward(u32),
+    Up(u32),
 }
-type Command = (Direction, u32);
 
 pub fn parse_input(file_bytes: &[u8]) -> SolverInput {
     let mut commands = vec![];
@@ -15,17 +14,15 @@ pub fn parse_input(file_bytes: &[u8]) -> SolverInput {
     let mut subslice = file_bytes;
     while subslice.len() > 0 {
         let character = subslice[0];
-        let direction = match character {
-            b'd' => Direction::Down,
-            b'f' => Direction::Forward,
-            b'u' => Direction::Up,
-            _ => panic!(),
-        };
-
         subslice = skip_ascii_whitespace(skip_to_ascii_whitespace(subslice));
         let (magnitude, new_subslice) = scan_ascii_to_u32(subslice);
 
-        commands.push((direction, magnitude));
+        commands.push(match character {
+            b'd' => Command::Down(magnitude),
+            b'f' => Command::Forward(magnitude),
+            b'u' => Command::Up(magnitude),
+            _ => panic!(),
+        });
 
         subslice = skip_ascii_whitespace(new_subslice);
     }
@@ -37,10 +34,10 @@ pub fn solve_part1(input: &SolverInput) -> u32 {
     let mut depth = 0;
     let mut distance = 0;
     for command in input {
-        match command.0 {
-            Direction::Down => depth += command.1,
-            Direction::Forward => distance += command.1,
-            Direction::Up => depth -= command.1,
+        match command {
+            Command::Down(val) => depth += val,
+            Command::Forward(val) => distance += val,
+            Command::Up(val) => depth -= val,
         }
     }
     return depth * distance;
@@ -51,13 +48,13 @@ pub fn solve_part2(input: &SolverInput) -> u32 {
     let mut depth = 0;
     let mut distance = 0;
     for command in input {
-        match command.0 {
-            Direction::Down => aim += command.1,
-            Direction::Forward => {
-                distance += command.1;
-                depth += aim * command.1;
+        match command {
+            Command::Down(val) => aim += val,
+            Command::Forward(val) => {
+                distance += val;
+                depth += aim * val;
             },
-            Direction::Up => aim -= command.1,
+            Command::Up(val) => aim -= val,
         }
     }
     return depth * distance;
