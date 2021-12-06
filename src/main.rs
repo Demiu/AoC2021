e55e95e24877a639617841af1bd74224fa17bdba
Demiu <miazgapow@gmail.com>
2021-12-06 10:59:02 +0100

    Make bigboy run day 5 only

--- src/main.rs
+++ src/main.rs
@@ -19,14 +19,14 @@ fn main() {
     let day3in = include_bytes!("../input/3/input.txt");
     let day3parsed = day3::parse_input(day3in).expect("Failed to parse input file for day 3");
     println!("Day 3 Part 1: {}", day3::solve_part1(&day3parsed));
-    println!("Day 3 Part 2: {}", day3::solve_part2(&day3parsed));*/
+    println!("Day 3 Part 2: {}", day3::solve_part2(&day3parsed));
 
     let day4in = include_bytes!("../input/4/bigboy.txt");
     let day4parsed = day4::parse_input(day4in).expect("Failed to parse input file for day 4");
     println!("Day 4 Part 1: {}", day4::solve_part1(&day4parsed));
-    println!("Day 4 Part 2: {}", day4::solve_part2(&day4parsed));
+    println!("Day 4 Part 2: {}", day4::solve_part2(&day4parsed));*/
 
-    let day5in = include_bytes!("../input/5/input.txt");
+    let day5in = include_bytes!("../input/5/bigboy.txt");
     let day5parsed = day5::parse_input(day5in).expect("Failed to parse input file for day 4");
     println!("Day 5 Part 1: {}", day5::solve_part1(&day5parsed));
     println!("Day 5 Part 2: {}", day5::solve_part2(&day5parsed));
