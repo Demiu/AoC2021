a0f121a99594664a3e6798c2a7c1d231600b0bbd
Demiu <miazgapow@gmail.com>
2022-01-28 10:00:23 +0100

    Day 10 tests

--- src/days/day10.rs
+++ src/days/day10.rs
@@ -8,9 +8,25 @@ use rayon::iter::{IntoParallelIterator, IntoParallelRefIterator, ParallelIterato
 type ParserOutput = Vec<Line>;
 type SolverInput = [Line];
 
+#[derive(Debug, PartialEq)]
 pub enum Line {
     Incomplete(Vec<u8>),
     Corrupted(u32),
+}
+
+fn autocomplete_score(to_close: &[u8]) -> u64 {
+    let mut score = 0;
+    for bracket in to_close.iter().rev() {
+        score *= 5;
+        score += match *bracket {
+            b'(' => 1,
+            b'[' => 2,
+            b'{' => 3,
+            b'<' => 4,
+            _ => unreachable!(),
+        }
+    }
+    score
 }
 
 pub fn parse_input(file: &[u8]) -> anyhow::Result<ParserOutput> {
@@ -64,21 +80,6 @@ pub fn solve_part1(input: &SolverInput) -> u32 {
 }
 
 pub fn solve_part2(input: &SolverInput) -> u64 {
-    fn autocomplete_score(to_close: &[u8]) -> u64 {
-        let mut score = 0;
-        for bracket in to_close.iter().rev() {
-            score *= 5;
-            score += match *bracket {
-                b'(' => 1,
-                b'[' => 2,
-                b'{' => 3,
-                b'<' => 4,
-                _ => unreachable!(),
-            }
-        }
-        score
-    }
-
     let mut scores: Vec<_> = input
         .par_iter()
         .filter_map(|line| {
@@ -91,4 +92,63 @@ pub fn solve_part2(input: &SolverInput) -> u64 {
         .collect();
     scores.sort_unstable();
     scores[scores.len() / 2] // scores are always odd in number
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const EXAMPLE: &[u8] = concat!(
+        "[({(<(())[]>[[{[]{<()<>>\n",
+        "[(()[<>])]({[<{<<[]>>(\n",
+        "{([(<{}[<>[]}>{[]{[(<()>\n",
+        "(((({<>}<{<{<>}{[]{[]{}\n",
+        "[[<[([]))<([[{}[[()]]]\n",
+        "[{[{({}]{}}([{[{{{}}([]\n",
+        "{<[[]]>}<{[{[{[]{()[[[]\n",
+        "[<(<(<(<{}))><([]([]()\n",
+        "<{([([[(<>()){}]>(<<{{\n",
+        "<{([{{}}[<[[[<>{}]]]>[]]\n",
+    ).as_bytes();
+
+    #[test]
+    fn parse_example() {
+        let parsed = parse_input(EXAMPLE);
+        assert!(parsed.is_ok(), "Failed parsing example input");
+        let parsed = parsed.unwrap();
+
+        assert_eq!(parsed[2], Line::Corrupted(1197));
+        assert_eq!(parsed[4], Line::Corrupted(3));
+        assert_eq!(parsed[5], Line::Corrupted(57));
+        assert_eq!(parsed[7], Line::Corrupted(3));
+        assert_eq!(parsed[8], Line::Corrupted(25137));
+        for i in 0..parsed.len() {
+            if let Ok(_) = [2,4,5,7,8].binary_search(&i) {
+                continue;
+            }
+            assert!(matches!(parsed[i], Line::Incomplete(_)));
+        }
+    }
+
+    crate::macros::make_test_for_day!(example, EXAMPLE, 26397, 288957);
+
+    #[test]
+    fn part2_example_scores() {
+        let parsed = parse_input(EXAMPLE);
+        assert!(parsed.is_ok(), "Failed parsing example input");
+        let parsed = parsed.unwrap();
+
+        let get_incomplete = |idx| {
+            match &parsed[idx] {
+                Line::Incomplete(v) => v,
+                _ => panic!("Trying to extract not from an incomplete line"),
+            }
+        };
+
+        assert_eq!(autocomplete_score(&get_incomplete(0)), 288957);
+        assert_eq!(autocomplete_score(&get_incomplete(1)), 5566);
+        assert_eq!(autocomplete_score(&get_incomplete(3)), 1480781);
+        assert_eq!(autocomplete_score(&get_incomplete(6)), 995444);
+        assert_eq!(autocomplete_score(&get_incomplete(9)), 294);
+    }
 }
