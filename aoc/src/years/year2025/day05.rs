df45cf6a1b4dcf08ea3fcc8b96b10fcd36a8cfdd
Demiu <miazgapow@gmail.com>
2025-12-06 15:26:07 +0100

    2025 day 5 optimizations

--- aoc/src/years/year2025/day05.rs
+++ aoc/src/years/year2025/day05.rs
@@ -10,7 +10,7 @@ use crate::{parse::parse_unsigned, traits::Union};
 type SolverInput = (Vec<RangeInclusive<u64>>, Vec<u64>);
 
 pub fn parse_input(file: &[u8]) -> Result<SolverInput> {
-    separated_pair(
+    let (_, (ranges_raw, ingredients)) = separated_pair(
         separated_list1(
             tag(b"\n"),
             map(
@@ -21,25 +21,13 @@ pub fn parse_input(file: &[u8]) -> Result<SolverInput> {
         tag(b"\n\n"),
         separated_list1(tag(b"\n"), parse_unsigned),
     )(file)
-    .map_err(|_| anyhow!("Failed parsing cells"))
-    .map(|t| t.1)
-}
+    .map_err(|_| anyhow!("Failed parsing cells"))?;
 
-pub fn solve_part1(input: &SolverInput) -> usize {
-    let (ranges, ingredients) = (&input.0[..], &input.1[..]);
-    ingredients
-        .iter()
-        .filter(|id| ranges.iter().any(|r| r.contains(id)))
-        .count()
-}
-
-pub fn solve_part2(input: &SolverInput) -> u64 {
-    type Rri<'a> = &'a RangeInclusive<u64>;
-    let mut ranges = Vec::new();
-    for new in input.0.iter().cloned() {
+    let mut ranges = Vec::<RangeInclusive<_>>::new();
+    for new in ranges_raw {
         let mut to_insert = new;
         loop {
-            let candidate = ranges.partition_point(|r: Rri| r.end() < to_insert.start());
+            let candidate = ranges.partition_point(|r| r.end() < to_insert.start());
             if candidate == ranges.len() {
                 ranges.push(to_insert);
                 break;
@@ -56,8 +44,22 @@ pub fn solve_part2(input: &SolverInput) -> u64 {
             }
         }
     }
-    ranges
-        .into_iter()
+
+    Ok((ranges, ingredients))
+}
+
+pub fn solve_part1(input: &SolverInput) -> usize {
+    let (ranges, ingredients) = (&input.0[..], &input.1[..]);
+    ingredients
+        .iter()
+        .filter(|id| ranges.iter().any(|r| r.contains(id)))
+        .count()
+}
+
+pub fn solve_part2(input: &SolverInput) -> u64 {
+    input.0
+        .iter()
+        .cloned()
         .map(RangeInclusive::into_inner)
         .map(|(l, h)| h - l + 1)
         .sum()
@@ -80,7 +82,7 @@ mod test {
         assert_eq!(
             parsed,
             (
-                [3..=5, 10..=14, 16..=20, 12..=18,].into(),
+                [3..=5, 10..=20,].into(),
                 [1, 5, 8, 11, 17, 32].into()
             )
         );
